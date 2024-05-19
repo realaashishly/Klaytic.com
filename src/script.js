629d358f4c7004a7fa0c36659733bcf6647dabf1
Aashish Singh <11aashish.singh@gmail.com>
2024-05-20 02:31:03 +0530

    responsiveness added

--- src/script.js
+++ src/script.js
@@ -1,10 +1,14 @@
-gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(ScrollTrigger);
 const container = document.querySelector(".hero__images-wrapper");
+
+const standardBtn = document.getElementById("standardBtn");
+const proBtn = document.getElementById("proBtn");
+
+const standardPrice = document.getElementById("standard-membership");
+const proPrice = document.getElementById("pro-membership");
 
 document.addEventListener("DOMContentLoaded", function () {
   const navLinks = document.querySelectorAll(".floating-nav__link");
-
-  const scrollPosition = window.scrollY;
 
   navLinks.forEach((link) => {
     link.addEventListener("click", () => {
@@ -30,3 +34,40 @@ tl.to(container, {
     // markers: true
   },
 });
+
+function togglePro() {
+  proPrice.style.display = "block";
+  proBtn.classList.add("w--current");
+  standardPrice.style.display = "none";
+  standardBtn.classList.remove("w--current");
+}
+
+function toggleStandard() {
+  proPrice.style.display = "none";
+  proBtn.classList.remove("w--current");
+  standardPrice.style.display = "block";
+  standardBtn.classList.add("w--current");
+}
+
+const faqRows = document.querySelectorAll(".faq__row");
+
+faqRows.forEach((row) => {
+  const rowAnswer = row.querySelector(".faq__answer");
+  const rowArrow = row.querySelector(".faq__arrow");
+
+  row.addEventListener("click", () => {
+    const isAnswerVisible = rowAnswer.style.display !== "none";
+
+    // Toggle answer visibility
+    if (isAnswerVisible) {
+      rowAnswer.style.display = "none";
+      rowArrow.style.transform = "rotate(0deg)";
+    } else {
+      rowAnswer.style.display = "block";
+      rowArrow.style.transform = "rotate(180deg)";
+    }
+  });
+});
+
+proBtn.addEventListener("click", togglePro);
+standardBtn.addEventListener("click", toggleStandard);
