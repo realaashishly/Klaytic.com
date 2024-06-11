adaf647e0eb91a5744d43d035ca06c021d31f33d
Aashish Singh <11aashish.singh@gmail.com>
2024-06-11 17:15:01 +0530

    added content

--- src/script.js
+++ src/script.js
@@ -1,58 +1,34 @@
 gsap.registerPlugin(ScrollTrigger)
-const container = document.querySelector('.hero__images-wrapper')
 
+const container = document.querySelector('.hero__images-wrapper')
 const standardBtn = document.getElementById('standardBtn')
 const proBtn = document.getElementById('proBtn')
-
 const standardPrice = document.getElementById('standard-membership')
 const proPrice = document.getElementById('pro-membership')
-
 const links = document.querySelectorAll('nav a')
 const light = document.querySelector('nav .spotLight')
 let currentActive = document.querySelector('nav a.active')
-
 var x = window.matchMedia('(max-width: 700px)')
 
-function moveSpotlight(target) {
-    // Adjust the size of the spotlight to match the target link
-    light.style.width = `${target.offsetWidth}px`
-    // Center the spotlight over the target link
-    light.style.left = `${target.offsetLeft}px`
-}
+// function moveSpotlight(target) {
+//     light.style.width = `${target.offsetWidth}px`
+//     light.style.left = `${target.offsetLeft}px`
+// }
 
 links.forEach((link) => {
     link.addEventListener('mouseover', (e) => {
-        // Remove active class from the current active link
         if (currentActive) {
             currentActive.classList.remove('active')
         }
-        // Add active class to the clicked link
         e.target.classList.add('active')
         currentActive = e.target
         moveSpotlight(e.target)
     })
 })
 
-// Initialize the spotlight position based on the active link
-if (currentActive) {
-    moveSpotlight(currentActive)
-}
-
-// document.addEventListener("DOMContentLoaded", function () {
-//   const navLinks = document.querySelectorAll(".floating-nav__link");
-
-//   navLinks.forEach((link) => {
-//     link.addEventListener("click", () => {
-//       // Remove 'active-link' class from all links
-//       navLinks.forEach((otherLink) => {
-//         otherLink.classList.remove("w--current");
-//       });
-
-//       // Add 'active-link' class to the clicked link
-//       link.classList.add("w--current");
-//     });
-//   });
-// });
+// if (currentActive) {
+//     moveSpotlight(currentActive)
+// }
 
 function scrollHandler() {
     if (!x.matches) {
@@ -64,7 +40,6 @@ function scrollHandler() {
                 start: 'top bottom',
                 end: '+500px',
                 scrub: true,
-                // markers: true
             },
         })
     }
@@ -74,19 +49,7 @@ x.addEventListener('change', function () {
     scrollHandler(x)
 })
 
-function togglePro() {
-    proPrice.style.display = 'block'
-    proBtn.classList.add('w--current')
-    standardPrice.style.display = 'none'
-    standardBtn.classList.remove('w--current')
-}
 
-function toggleStandard() {
-    proPrice.style.display = 'none'
-    proBtn.classList.remove('w--current')
-    standardPrice.style.display = 'block'
-    standardBtn.classList.add('w--current')
-}
 
 const faqRows = document.querySelectorAll('.faq__row')
 
@@ -97,7 +60,6 @@ faqRows.forEach((row) => {
     row.addEventListener('click', () => {
         const isAnswerVisible = rowAnswer.style.display !== 'none'
 
-        // Toggle answer visibility
         if (isAnswerVisible) {
             rowAnswer.style.display = 'none'
             rowArrow.style.transform = 'rotate(0deg)'
@@ -108,5 +70,4 @@ faqRows.forEach((row) => {
     })
 })
 
-proBtn.addEventListener('click', togglePro)
-standardBtn.addEventListener('click', toggleStandard)
+
